bbb789f5b54098b40c132774e05412e2eb9829dd
Paula Sazonov <paula.sazonov@wunderdog.fi>
2023-06-27 16:32:11 +0300

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,23 @@
+defmodule Identicon do
+  @moduledoc """
+  Module for generating reproducable identicons from strings
+  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _rest]} = image
+    [r, g, b]
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
